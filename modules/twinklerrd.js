7434f0b8383af7b87b5cf5ee59e0ff497f77528a
dyf <dyf123456789@gmail.com>
2014-04-28 15:44:23 +0800

    RDD part5

--- modules/twinklerrd.js
+++ modules/twinklerrd.js
@@ -21,27 +21,27 @@ Twinkle.rrd = function twinklerrd() {
 Twinkle.rrd.callback=function rrdcallback(){
     var li_revs=$("ul#pagehistory li");
     li_revs.each(function(){
-        var li_rev=$(this);        
+        var li_rev=$(this);
         var rev_value=li_rev.find("input[name='oldid']").val();
-        
+
         var checkbox=$("<input>");
-        
+
         checkbox
             .attr("type","checkbox")
             .attr("name","selectrid")
             .addClass("tw-rrd-selectrid")
             .attr("value",rev_value);
-            
+
         var insert=li_rev.find("span.mw-history-histlinks");
-        
-        insert.after(checkbox);        
+
+        insert.after(checkbox);
     });
-    
+
     var Window = new Morebits.simpleWindow( 400, 300 );
     Window.setTitle( "批量申请特定版本删除删除" );
     //Window.setScriptName( "Twinkle" );
     //Window.addFooterLink( "Twinkle帮助", "WP:TW/DOC#delimages" );
-    
+
     var form = new Morebits.quickForm( Twinkle.rrd.callback.evaluate );
     form.append( {
         type: 'radio',
@@ -60,7 +60,7 @@ Twinkle.rrd.callback=function rrdcallback(){
             }
         ]
     } );
-    
+
     content_work_area =new Morebits.quickForm.element( {
                             type: 'field',
                             label: '移除内容',
@@ -90,7 +90,7 @@ Twinkle.rrd.callback=function rrdcallback(){
         ]
     } );
     form.append(content_work_area);
-    
+
     form.append( {
         type:'select',
         name:'Reason',
@@ -99,43 +99,43 @@ Twinkle.rrd.callback=function rrdcallback(){
         event:Twinkle.rrd.ReasonForOther,
         list:[
             {
-                type: 'option',                
-                label:'RD1：侵犯版权',                
-                value:'rd1'                
+                type: 'option',
+                label:'RD1：侵犯版权',
+                value:'rd1'
             },
             {
-                type: 'option',                
-                label:'RD2：针对个人、团体或组织的严重侮辱、贬低或攻击性材料',                
-                value:'rd1'                
+                type: 'option',
+                label:'RD2：针对个人、团体或组织的严重侮辱、贬低或攻击性材料',
+                value:'rd1'
             },
             {
-                type: 'option',                
-                label:'RD3：纯粹的扰乱性内容',                
-                value:'rd2'                
+                type: 'option',
+                label:'RD3：纯粹的扰乱性内容',
+                value:'rd2'
             },
             {
-                type: 'option',                
-                label:'RD4：非公开的私人信息',                
-                value:'rd3'                
+                type: 'option',
+                label:'RD4：非公开的私人信息',
+                value:'rd3'
             },
             {
-                type: 'option',                
-                label:'RD4：非公开的私人信息：用户编辑未登录，泄漏IP地址',                
-                value:'rd4'                
+                type: 'option',
+                label:'RD4：非公开的私人信息：用户编辑未登录，泄漏IP地址',
+                value:'rd4'
             },
             {
-                type: 'option',                
-                label:'RD5：删除守则下的有效删除，使用RevisionDelete执行',                
-                value:'rd5'                
+                type: 'option',
+                label:'RD5：删除守则下的有效删除，使用RevisionDelete执行',
+                value:'rd5'
             },
             {
-                type: 'option',                
-                label:'RD6：版本删除校正',                
-                value:'rd6'                
+                type: 'option',
+                label:'RD6：版本删除校正',
+                value:'rd6'
             },
             {
-                type: 'option',                
-                label:'其他原因',                
+                type: 'option',
+                label:'其他原因',
                 value:'other'
             }
         ]
@@ -148,12 +148,12 @@ Twinkle.rrd.callback=function rrdcallback(){
         label: '请输入其他理由'
     } );
     */
-    
+
     form.append( { type:'submit' } );
     var result = form.render();
     Window.setContent(result);
-    Window.display();   
-    
+    Window.display();
+
     var evt = document.createEvent( "Event" );
     evt.initEvent( 'change', true, true );
     result.category.dispatchEvent( evt );
@@ -166,7 +166,7 @@ Twinkle.rrd.ReasonForOther=function rdReasonOther(e){
     var $reason=$(Morebits.quickForm.getElements(form,"Reason")[0]);
     var reason=$reason.find("option:selected").first().val();
     var div = new Morebits.quickForm.element({ type: 'div', id: 'ReasonOther' });
-    
+
     if(reason=='other')
     {
         div.append( {
@@ -177,28 +177,44 @@ Twinkle.rrd.ReasonForOther=function rdReasonOther(e){
     }
     else
     {
-        
+
     };
-    $divReasonOther.replaceWith(div.render()); 
+    $divReasonOther.replaceWith(div.render());
+};
+
+Twinkle.rrd.getSelectArray=function(){
+    var rev_inputs=$("input.tw-rrd-selectrid");
+    var rev_values=new Array();
+    var count=0;
+    rev_inputs.each(function(){
+        var rev_input=$(this);
+        if(rev_input.is(":checked"))
+            rev_values[count++]=$(this).val();
+    });
+    rev_values.sort();
+    
+    return rev_values;    
 };
 
 Twinkle.rrd.callbacks = {
     main: function( pageobj ) {
         var params = pageobj.getCallbackParameters();
-        var rev_values=$("input.tw-rrd-selectrid");
         
+        var rev_values=Twinkle.rrd.getSelectArray();
+
         wikipedia_page = new Morebits.wiki.page("Wikipedia:修订版本删除请求", "添加项目");
         wikipedia_page.setFollowRedirect(true);
         wikipedia_page.setCallbackParameters(params);
         var addtext="";
-        
-        
+
+
         wikipedia_page.setAppendText(addtext);
         wikipedia_page.setEditSummary("添加[[" + Morebits.pageNameNorm + "]]的版本提出。" + Twinkle.getPref('summaryAd'));
-        
+
         //wikipedia_page.load(Twinkle.xfd.callbacks.afd.todaysList);
         wikipedia_page.append();
-        rev_values.remove();
+
+        rev_inputs.remove();
     }
 };
 
@@ -206,16 +222,65 @@ Twinkle.rrd.callback.evaluate = function twrrdCallbackEvaluate(e) {
     var form = e.target;
     var params = {};
     
+    var rev_values=Twinkle.rrd.getSelectArray();
+    //console.log(form);
+    var selectmode=$(form).find("input[name=SelectMode]:checked").val();
+    //console.log(selectmode);
+    //console.log(rev_values);
+    if(rev_values.length<=0)
+    {
+        alert("请至少选择一个项目");
+        return;
+    }
+    else
+    {
+        if(selectmode=="2")
+        {
+            if(rev_values.length==1)
+            {
+                alert("按照前后版本选择，请选择超过2个选项");
+                return;
+            }
+            var max,min;
+            if(rev_values.length==2)
+            {
+                min=rev_values[0];
+                max=rev_values[1];
+            }
+            else
+            {
+                min=rev_values[0];
+                max=rev_values[rev_values.length-1];
+                
+                var query={
+                    'action':'query',
+                    'prop':'revisions',
+                    'rvprop':'ids',
+                    'rvstartid':,
+                    'rvendid':,
+                    'titles': mw.config.get("wgPageName"),
+                    'rvlimit' : Twinkle.getPref('batchMax') // the max for sysops
+                };
+                var apiquery=new Morebits.wiki.api('抓取历史', query,function(self){
+                
+                
+                });
+            }
+        }        
+    }
+    
+    /*
     Morebits.simpleWindow.setButtonsEnabled( false );
     Morebits.status.init( form );
 
     Morebits.wiki.actionCompleted.notice = "提交完成，在几秒内刷新页面";
-    
+
     Morebits.wiki.addCheckpoint();
     var wikipedia_page = new Morebits.wiki.page('Wikipedia:修订版本删除请求', "正在提交");
-    wikipedia_page.setCallbackParameters(params);   
+    wikipedia_page.setCallbackParameters(params);
     wikipedia_page.load(Twinkle.rrd.callbacks.main);
     Morebits.wiki.removeCheckpoint();
+    */
 };
 })(jQuery);
 //</nowiki>
