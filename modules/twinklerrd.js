0c5c5783ce257b3e028f98d07596e3d6e53cffa1
dyf <dyf123456789@gmail.com>
2014-05-05 15:17:49 +0800

    change the format of summary
    fixed some word

--- modules/twinklerrd.js
+++ modules/twinklerrd.js
@@ -252,7 +252,7 @@ Twinkle.rrd.callbacks = {
         ]);
         //console.log(addtext);
         wikipedia_page.setAppendText(addtext);
-        wikipedia_page.setEditSummary("特定版本删除：[[" + Morebits.pageNameNorm + "]]。" + Twinkle.getPref('summaryAd'));
+        wikipedia_page.setEditSummary("特定版本删除申请：[[" + Morebits.pageNameNorm + "]]。" + Twinkle.getPref('summaryAd'));
 
         wikipedia_page.append();
     }
